c0d5890bd4cc273c2a54f246c4ddc8ee52594a10
SR Melody <srmelody@gmail.com>
2018-06-01 07:40:21 -0400

    Adding value field to the PickerBase type

--- lib/src/_shared/PickerBase.d.ts
+++ lib/src/_shared/PickerBase.d.ts
@@ -1,10 +1,11 @@
 import { MaterialUiPickersDate } from '../typings/date'
 import { DateTextFieldProps } from './ModalDialog';
+import { DateType } from '../constants/prop-types'
 
 export interface PickerBaseProps {
     onChange: (date: MaterialUiPickersDate) => void;
     autoOk?: boolean;
     ampm?: boolean;
     pickerRef?: (e: Element) => void;
-
+    value: DateType;
 }
